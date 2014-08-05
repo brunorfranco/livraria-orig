2438a2c5a32bad716931b95337f273f7fb97350e
brunorfranco <brunozo_rf@hotmail.com>
2014-08-05 13:39:36 -0300

    JSF + JPA

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,27 +1,75 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
 
 	public Livro getLivro() {
 		return livro;
 	}
 
-	public void gravar() {
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+	    String valor = value.toString();
+	    if (!valor.startsWith("1")) {
+	        throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+	    }
+	}
+	
+	public String gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+		return "";
+	}
+
+	public String gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesMessage message = new FacesMessage("Livro deve ter pelo menos um Autor.");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage("autor", message);
+            return "";
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		this.livro = new Livro();
+		return "";
 	}
 
 }
